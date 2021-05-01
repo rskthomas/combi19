d2218caa24b27dc53646ea1d0b2680ada5a45bc0
agusisabella <agustinaisabellav@gmail.com>
2021-05-01 18:13:04 -0300

    alta de choferes funcionando ultimo

---
+++ app/Http/Controllers/ChoferesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChoferesController extends Controller
+{
+    public function listarChoferes()
+    {
+  //terminar
+    }
+
+    
+
+
+   
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,17 @@
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+<!-- MOVER ESTO AL HOME DEL  ADMIN -->
+@if(session()->has('popup'))
+
+<div class="alert alert-success" role="alert">
+  Se ha creado con exito un usuario chofer 
+</div>
+
+
+@endif
+
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -16,3 +29,7 @@
         </div>
     </div>
 </x-app-layout>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
