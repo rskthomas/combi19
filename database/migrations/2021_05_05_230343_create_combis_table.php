9eef0506d69dd66b476dd454a9c688c5afe01b30
rthomas <rusieckithomas@gmail.com>
2021-05-05 21:26:38 -0300

    Combi model and migration added

---
+++ database/migrations/2021_05_05_230343_create_combis_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCombisTable extends Migration
+{
+    
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('combis', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->String('patente');
+            $table->integer('asientos');
+            $table->boolean('super_comoda');
+
+            $table->foreignId('chofer_id')
+                  ->nullable()
+                  ->onDelete('SET NULL')
+                  ->constrained('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('combis');
+    }
+}
