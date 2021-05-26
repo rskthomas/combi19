129507f371466b140b9dd2cadbcf46e482c4c4ad
rthomas <rusieckithomas@gmail.com>
2021-05-26 00:14:21 -0300

    x-row used

--- resources/views/entidades/lugar/info.blade.php
+++ resources/views/entidades/lugar/info.blade.php
@@ -13,7 +13,7 @@
 
     @endif
 
-    
+
     @if(session()->has('lugarmodificado'))
     <div class="alert alert-success text-center" role="alert">
         <span>Lugar modificado con exito </span>
@@ -22,42 +22,37 @@
     @endif
 
 
-   
+
 
     <div class="py-8 col-md-5 mx-auto ">
         <div class="bg-white border-b border-gray-200 ">
 
             <div class="container p-2 ">
-                <!-- a row -->
-                <div class="row p-4 ">
-                    <div class="col-sm font-semibold ">Terminal </div>
-                    <div class="col-sm ">
-                        <div class="col-sm-9 text-secondary text-left">
-                            {{ $lugar->nombre}}
-                        </div>
-                    </div>
-                </div>
-                <hr />
-                <!-- a row -->
-                <div class="row p-4 ">
-                    <div class="col-sm font-semibold ">Provincia </div>
-                    <div class="col-sm ">
-                        <div class="col-sm-9 text-secondary text-left">
-                            {{ $lugar->provincia }}
-                        </div>
-                    </div>
-                </div>
-                <hr />
 
                 <!-- a row -->
-                <div class="row p-3 mt-3">
-                    <div class="col-sm "> Lugar registrado el </div>
-                    <div class="col-sm ">
-                        <div class="col-sm-9 text-secondary text-left">
-                            {{ $lugar->created_at  }}
-                        </div>
-                    </div>
-                </div>
+                <x-row>
+                    <x-slot name="type">
+                        Terminal
+                    </x-slot>
+                    {{ $lugar->nombre}}
+                </x-row>
+                <hr />
+                 <!-- a row -->
+                 <x-row>
+                    <x-slot name="type">
+                        Provincia
+                    </x-slot>
+                    {{ $lugar->provincia}}
+                </x-row>
+                <hr />
+                <!-- a row -->
+                 <x-row>
+                    <x-slot name="type">
+                        Lugar registrado el
+                    </x-slot>
+                    {{ $lugar->created_at }}
+                </x-row>
+
             </div>
         </div>
     </div>
