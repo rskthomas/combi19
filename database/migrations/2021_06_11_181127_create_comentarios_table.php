646940cfa2ecfdc6cc6f331e2f6487349107ff5d
emilio1986 <emilio_1986win@hotmail.com>
2021-06-11 15:35:27 -0300

    modelo+migracion+entidad

---
+++ database/migrations/2021_06_11_181127_create_comentarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("contenido");
+            $table->foreignId('user_id')
+                ->nullable()
+                ->onDelete('SET NULL')
+                ->constrained('users');
+                $table->date('fecha');
+            $table->time('hora');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
