962c5614a4e09239280838644a1cfb8e15bc1373
rthomas <rusieckithomas@gmail.com>
2021-06-15 16:59:58 -0300

    seeder con admin1 added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +18,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LaratrustSeeder::class);
+
+        $user = User::create([
+            'name' => 'elAdmin',
+            'email' => 'admin1@gmail.com',
+            'password' => Hash::make('12345678'),
+            'birthdate'=> '06-06-1665',
+            'isGold' => false,
+        ]);
+        $user->attachRole('administrator');
+        event(new Registered($user));
 
     }
 }
