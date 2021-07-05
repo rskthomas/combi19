0b2e24ceb287470a6cc55feb64316bd4ee925d28
Your Name <you@example.com>
2021-07-05 13:16:21 -0300

    finalizo_suViaje_?

--- app/Http/Controllers/ViajeController.php
+++ app/Http/Controllers/ViajeController.php
@@ -231,7 +231,7 @@ class ViajeController extends Controller
             'id_chofer' => $viaje->ruta->combi->chofer->id,
             'mail_chofer' => $viaje->ruta->combi->chofer->email,
         ]);
-
+    //chequear esta condicion de los comentarios con el viaje finalizado porque no esta andando
         $viaje->pasajes()->where('estado', '=', 'activo')->where('viaje_id', '=', $viaje->id)
               ->each(function ($pasaje, $key) {
             $pasaje->usuario->realizoPasaje();
@@ -240,6 +240,8 @@ class ViajeController extends Controller
         $pasajes = Pasaje::where('estado', '=', 'pendiente')->where('viaje_id', '=', $viaje->id)->update(['estado' => 'ausente']);
 
         $pasajes = Pasaje::where('estado', '=', 'activo')->where('viaje_id', '=', $viaje->id)->update(['estado' => 'finalizado']);
+
+        
 
         $pasajes = Pasaje::where('viaje_id', '=', $viaje->id)->update(['viaje_id' => null]);
 
